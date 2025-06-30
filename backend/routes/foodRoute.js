7ebfc66ec2b3a67993e0b96680f5b5e063e6faa6
suhacodes1 <suha.fathima.au@gmail.com>
2025-06-30 13:54:01 +1000

    created add, remove and list food api

--- backend/routes/foodRoute.js
+++ backend/routes/foodRoute.js
@@ -1,22 +1,24 @@
-import express from "express"
-import { addFood } from "../controllers/foodController.js"
-import multer from "multer"
+import express from "express";
+import { addFood,listFood,removeFood } from "../controllers/foodController.js";
+import multer from "multer";
+import path from "path";
 
 const foodRouter = express.Router();
 
 // Image Storage Engine
-
 const storage = multer.diskStorage({
-    destination: "uploads",
-    filename:(req,file,cb)=>{
-return cb(null,`${Date,now()}${file.originalname}`)
-    }
-})
+  destination: (req, file, cb) => {
+    cb(null, "uploads/"); // Make sure the 'uploads' folder exists
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  },
+});
 
-const upload = multer({storage:storage})
+const upload = multer({ storage });
 
-foodRouter.post("/add", upload.single("image"),addFood)
-
-
+foodRouter.post("/add", upload.single("image"), addFood);
+foodRouter.get("/list",listFood)
+foodRouter.post("/remove", removeFood)
 
 export default foodRouter;
