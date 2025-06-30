f18384e8469a7db7e7a964e77cf31444205a9262
suhacodes1 <suha.fathima.au@gmail.com>
2025-06-30 14:36:57 +1000

    navbar do ne

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import Navbar from './components/Navbar/Navbar'
+import SideBar from './components/sidebar/SideBar'
 
 const App = () => {
   return (
     <div>
-      
-    </div>
+      <Navbar/>
+      <hr/>
+      <div className ="app-content">
+<SideBar/>
+      </div>
+    </div >
   )
 }
 
